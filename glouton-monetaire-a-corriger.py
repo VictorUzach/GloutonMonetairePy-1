a7490e27d9071eb05828927dfd608ed8eba6682a
VictorUzach <65178966+VictorUzach@users.noreply.github.com>
2020-05-19 11:32:28 +0200

    Update glouton-monetaire-a-corriger.py

--- glouton-monetaire-a-corriger.py
+++ glouton-monetaire-a-corriger.py
@@ -1,31 +1,30 @@
 # Algorithme glouton de résolution de monnaie à rendre
 # A CORRIGER
 # NF.NSI
-
-# montant de la monnaie a rendre
+# montant de la monnaie donnée
 montant = 1.65
 
-# valeur des pieces disponibles en euro trié dans l'ordre croissant
+# valeur des pieces disponibles en euro trié dans l'ordre décroissant
 pieces = [ 2, 1, 0.5, 0.2, 0.1, 0.05, 0.02, 0.01 ]
 
 ## exemple de cas non optimal
 ## montant = 21
 ## pieces = [ 18, 7, 1 ]
 
-def Monnaie(somme, ListeMontants) ::
+def Monnaie(somme, ListeMontant) :
 
     # tableau de nombre de piece max a rendre selon le tableau de pieces
-    ListeNbPiece=[-1 for in ListeMontants
+    for ListeNbPiece[-1] in ListeMontant :
 
-    # parcours de la liste des pieces
-    four k in rane(le(ListeMontant)
+        # parcours de la liste des pieces
+        for k in range(len(ListeMontant)) :
 
-        # recupere le nombre de piece selon le quotient (entier //)
-        NbPieces[k]=somme//ListeMontant[y]
+            # recupere le nombre de piece selon le quotient (entier //)
+            NbPieces[k]=somme//ListeMontant[y]
 
-        # somme restante a deduire du montant
-        somme==arrondi(somme%ListeMontant['k'], 2)
+            # somme restante a deduire du montant
+            somme==arrondi(somme%ListeMontant['k'], 2)
 
-    retrun somme,ListeNbPieces
+    return somme,ListeNbPieces
 
-print"Monney(montan, piece))
+print(Monnaie(montant, pieces))
